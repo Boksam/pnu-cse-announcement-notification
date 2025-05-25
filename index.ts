b2633297382a51e0a59935a151c293d722875aa9
Park Junwoo <boksam1017@gmail.com>
2025-05-25 13:44:25 +0900

    implement notification

--- index.ts
+++ index.ts
@@ -9,6 +9,8 @@ import {
   seenTitles,
 } from "./src/store";
 
+dotenv.config();
+
 const app = Express();
 app.use(Express.json());
 
@@ -16,28 +18,9 @@ app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
-dotenv.config();
-
 cron.schedule("0 8 * * *", async () => {
   console.log("Running a task every day at 8:00 AM");
 
-  // Get locale date
-  const date = new Date();
-  const formattedDate = date.toISOString().split("T")[0];
-
-  const announcements = await fetchAnnouncements();
-  const newAnnouncements = announcements.filter((a) =>
-    isNewAnnouncement(a.title),
-  );
-
-  await sendEmail(announcements, formattedDate);
-  newAnnouncements.forEach((a) => addAnnouncementTitle(a.title));
-
-  console.log("Email sent successfully");
-  console.log("current seen titles:", seenTitles);
-});
-
-app.get("/", async (req, res) => {
   // Get locale date
   const date = new Date();
   const formattedDate = date.toISOString().split("T")[0];
